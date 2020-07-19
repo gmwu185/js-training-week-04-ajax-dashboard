1248a48e74a05223f624a7e3dde6703dc10903fc
gmwu185 <gmwu185@gmail.com>
2020-07-19 22:06:34 +0800

    完成登入頁與產品列表取得資料 AJAX

--- source/assets/js/components/bbcall.js
+++
@@ -1,4 +0,0 @@
-Vue.component('bbcall', {
-  template: `<div>請正確呈現訊息：{{ msg }}</div>`,
-  props: ['msg']
-});
--- source/assets/js/components/bbcall2.js
+++
@@ -1,4 +0,0 @@
-Vue.component('bbcall2', {
-  template: `<div>請正確呈現訊息：{{ outerMessage }}</div>`,
-  props: ['outerMessage']
-});
